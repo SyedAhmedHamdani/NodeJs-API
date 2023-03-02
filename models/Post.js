d4dac843167f1ddba22134d69622302b96042cd9
SyedAhmedHamdani <75111131+SyedAhmedHamdani@users.noreply.github.com>
2023-03-02 11:57:55 +0500

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -207,7 +207,7 @@ class Post {
        let addServerDb=`Insert into closet_app_info set user_id_main_server='${userId}',closet_id_main_server='${result.insertId}', closet_name='${closetName}',password='${password}',country='${country}',proxy='${RandomProxy}',update_status='${1}'`
        let data = await connection.promise().query(addServerDb);
 
-        return 'SuccessFul'
+        return RandomServerDatabase
              
      }
      static async updateLinkCloset(closetName,password,country,userId,serverDatabase) {
