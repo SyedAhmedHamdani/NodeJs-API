d393682860ee632617f2ef6cd1f490866d591950
SyedAhmedHamdani <75111131+SyedAhmedHamdani@users.noreply.github.com>
2023-03-08 21:50:52 +0500

    Fixes

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -12,52 +12,52 @@ exports.getUserLogs= async (req,res,next) => {
     let {userId,closetId,serverDatabase,flag} = req.body;
     let post = new Post(userId,closetId,serverDatabase,flag);
     post = await post.findLogs(); 
-    res.status(200).json({post})
+    res.status(200).json({post:post})
 }
 exports.getUser= async (req,res,next) => {
     let {email,password} = req.body;
     let [user,_] = await Post.findUser(email,password); 
-    res.status(200).json({user});
+    res.status(200).json({user:user});
 }
 exports.getUserCloset= async (req,res,next) => {
     let {userId} = req.body;
     let [closet,_] = await Post.findCloset(userId); 
-    res.status(200).json({closet});
+    res.status(200).json({closet:closet});
 }
 exports.addUserSchedule= async (req,res,next) => {
     let {userId,closetId,schedules,serverDatabase} = req.body;
     let schedule= await Post.addSchedule(userId,closetId,schedules,serverDatabase); 
-    res.status(200).json({schedule});
+    res.status(200).json({schedule:schedule});
 }
 exports.getUserSchedule= async (req,res,next) => {
     let {userId,closetId} = req.body;
     let [schedule,_]= await Post.getSchedule(userId,closetId); 
-    res.status(200).json(schedule);
+    res.status(200).json({schedule:schedule});
 }
 exports.signupNewUser= async (req,res,next) => {
     let {firstName,lastName,phoneNo,email,password} = req.body;
     let [user,_]= await Post.addUser(firstName,lastName,phoneNo,email,password); 
-    res.status(200).json(user);
+    res.status(200).json({user:user});
 }
 exports.addUserCard= async (req,res,next) => {
     let {holderName,cardNo,expiryDate} = req.body;
     let [card,_]= await Post.addCard(holderName,cardNo,expiryDate); 
-    res.status(200).json(card);
+    res.status(200).json({card:card});
 }
 exports.addNewSubscription= async (req,res,next) => {
     let {customerId,priceId} = req.body;
     let subscription= await Post.getSubscription(customerId,priceId); 
-    res.status(200).json(subscription);
+    res.status(200).json({subscription:subscription});
 }
 exports.addNewPaymentMethod= async (req,res,next) => {
     let {cardNo,exp_month,exp_year,cvc} = req.body;
     let payment_method= await Post.addPaymentMethod(cardNo,exp_month,exp_year,cvc); 
-    res.status(200).json(payment_method);
+    res.status(200).json({payment_method:payment_method});
 }
 exports.attachNewPaymentMethod= async (req,res,next) => {
     let {paymentMethodId,customerId} = req.body;
     let payment_method= await Post.attachPaymentMethod(paymentMethodId,customerId); 
-    res.status(200).json(payment_method);
+    res.status(200).json({payment_method:payment_method});
 }
 exports.getStripeProduct= async (req,res,next) => {
     let product= await Post.getProduct(); 
@@ -75,20 +75,20 @@ exports.cancelStripeSubscription= async (req,res,next) => {
 exports.linkCloset= async (req,res,next) => {
     let {closetName,password,country,userId} = req.body;
     let closet= await Post.linkCloset(closetName,password,country,userId); 
-    res.status(200).json(closet);
+    res.status(200).json({closet:closet});
 }
 exports.updateLinkCloset= async (req,res,next) => {
     let {closetName,password,country,userId,serverDatabase} = req.body;
     let closet= await Post.updateLinkCloset(closetName,password,country,userId,serverDatabase); 
-    res.status(200).json(closet);
+    res.status(200).json({closet:closet});
 }
 exports.getCurrentActivity= async (req,res,next) => {
     let {userId,closetId,serverDatabase} = req.body;
     let Activity= await Post.getCurrentActivity(userId,closetId,serverDatabase); 
-    res.status(200).json(Activity);
+    res.status(200).json({Activity:Activity});
 }
 exports.getStatsCount= async (req,res,next) => {
     let {userId,closetId,serverDatabase} = req.body;
     let Stats= await Post.getStatsCount(userId,closetId,serverDatabase); 
-    res.status(200).json(Stats);
+    res.status(200).json({Stats:Stats});
 }
